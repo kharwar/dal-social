604a341e95c231ea970e561e6076edacc05a6182
siddhxrthh <ksiddharth127@gmail.com>
2022-11-16 18:17:35 -0400

    event list fragment

--- app/src/main/java/com/example/dalsocial/fragment/EventListFragment.kt
+++ app/src/main/java/com/example/dalsocial/fragment/EventListFragment.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import com.example.dalsocial.R
 import android.view.ViewGroup
+import com.example.dalsocial.R
 
-class EventCardFragment : Fragment() {
+class EventListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_card, container, false)
+        return inflater.inflate(R.layout.fragment_event_list, container, false)
     }
 }
