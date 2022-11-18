306b39212a35481ed09990998487e38948dcdd1b
Hrishi <hr581440@dal.ca>
2022-11-18 13:10:59 -0400

    setup home navigation

--- app/src/main/java/com/example/dalsocial/HomeActivity.kt
+++ app/src/main/java/com/example/dalsocial/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -12,8 +13,34 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val navController: NavController = Navigation.findNavController(this, R.id.activity_home_nav_host_fragment);
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation);
-        Navigation.setViewNavController(bottomNavigationView, navController);
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_social_page -> {
+                    findNavController(R.id.activity_home_nav_host_fragment).navigate(R.id.socialFragment)
+                    true
+                }
+                R.id.menu_events_page -> {
+                    findNavController(R.id.activity_home_nav_host_fragment).navigate(R.id.eventsFragment)
+                    true
+                }
+                R.id.menu_groups_page -> {
+                    findNavController(R.id.activity_home_nav_host_fragment).navigate(R.id.eventsFragment)
+                    true
+                }
+                R.id.menu_chat_page -> {
+                    findNavController(R.id.activity_home_nav_host_fragment).navigate(R.id.chatFragment)
+                    true
+                }
+                R.id.menu_profile_page -> {
+                    findNavController(R.id.activity_home_nav_host_fragment).navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+
     }
 }
