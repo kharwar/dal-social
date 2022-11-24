16da4c27a2e527a0a002a8c52dbd6e64dc9a222b
Faiza Umatiya <faizaumatiya@dal.ca>
2022-11-23 23:04:12 -0400

    Implemented matches of the Social feature

--- app/src/main/java/com/example/dalsocial/fragment/EventListFragment.kt
+++ app/src/main/java/com/example/dalsocial/fragment/EventListFragment.kt
@@ -1,44 +1,44 @@
-package com.example.dalsocial.fragment
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.dalsocial.R
-import com.example.dalsocial.databinding.FragmentEventListBinding
-import com.example.dalsocial.model.Event
-import com.example.dalsocial.model.EventAdapter
-import com.example.dalsocial.model.EventPersistence
-import com.example.dalsocial.model.IEventPersistence
-import java.util.*
-import kotlin.collections.ArrayList
-
-class EventListFragment : Fragment() {
-
-    lateinit var binding: FragmentEventListBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentEventListBinding.inflate(layoutInflater)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        var eventsList: MutableList<Event> = ArrayList()
-        val eventPersistence: IEventPersistence = EventPersistence()
-
-        eventPersistence.getAllEvents() { events ->
-                val adapter = EventAdapter(events, this);
-                binding.eventList.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
-                binding.eventList.adapter = adapter
-        }
-
-
-    }
-}
+//package com.example.dalsocial.fragment
+//
+//import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import com.example.dalsocial.R
+//import com.example.dalsocial.databinding.FragmentEventListBinding
+//import com.example.dalsocial.model.Event
+//import com.example.dalsocial.model.EventAdapter
+//import com.example.dalsocial.model.EventPersistence
+//import com.example.dalsocial.model.IEventPersistence
+//import java.util.*
+//import kotlin.collections.ArrayList
+//
+//class EventListFragment : Fragment() {
+//
+//    lateinit var binding: FragmentEventListBinding
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        binding = FragmentEventListBinding.inflate(layoutInflater)
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        var eventsList: MutableList<Event> = ArrayList()
+//        val eventPersistence: IEventPersistence = EventPersistence()
+//
+//        eventPersistence.getAllEvents() { events ->
+//                val adapter = EventAdapter(events, this);
+//                binding.eventList.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+//                binding.eventList.adapter = adapter
+//        }
+//
+//
+//    }
+//}
---
+++ app/src/main/java/com/example/dalsocial/fragment/MatchesViewHolders.kt
@@ -0,0 +1,31 @@
+package com.example.dalsocial.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dalsocial.R
+
+
+class MatchesViewHolders(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    var mMatchId: TextView
+    var mMatchName: TextView
+    var mMatchImage: ImageView
+    override fun onClick(view: View) {
+        val intent = Intent(view.context, ChatFragment::class.java)
+        val b = Bundle()
+        b.putString("matchId", mMatchId.text.toString())
+        intent.putExtras(b)
+        view.context.startActivity(intent)
+    }
+
+    init {
+        itemView.setOnClickListener(this)
+        mMatchId = itemView.findViewById<View>(R.id.Matchid) as TextView
+        mMatchName = itemView.findViewById<View>(R.id.MatchName) as TextView
+        mMatchImage = itemView.findViewById<View>(R.id.MatchImage) as ImageView
+    }
+}
+//Reference: https://github.com/LukeKotlin/tinder-clone-kotlin
