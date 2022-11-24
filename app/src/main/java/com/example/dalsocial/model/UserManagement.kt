8cf9dd53ec1bb024eb01803f50a280bc5b332794
Hrishi <hr581440@dal.ca>
2022-11-24 11:57:26 -0400

    add reset password functionality

--- app/src/main/java/com/example/dalsocial/model/UserManagement.kt
+++ app/src/main/java/com/example/dalsocial/model/UserManagement.kt
@@ -1,6 +1,11 @@
 package com.example.dalsocial.model
 
+import android.content.Intent
 import android.net.Uri
+import androidx.activity.result.ActivityResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import java.util.*
@@ -61,7 +66,7 @@ class UserManagement : IUserManagement, Observable() {
     override fun loginWithEmail(
         email: String,
         password: String,
-        function: (status: Boolean) -> Unit
+        result: (status: Boolean) -> Unit
     ) {
         if (currentUser == null) {
             auth!!.signInWithEmailAndPassword(email, password)
@@ -70,9 +75,9 @@ class UserManagement : IUserManagement, Observable() {
                         currentUser = auth!!.currentUser
                         setChanged()
                         notifyObservers()
-                        function(true)
+                        result(true)
                     } else {
-                        function(false)
+                        result(false)
                     }
                 }
         }
@@ -81,7 +86,7 @@ class UserManagement : IUserManagement, Observable() {
     override fun registerWithEmail(
         email: String,
         password: String,
-        function: (status: Boolean) -> Unit
+        result: (status: Boolean) -> Unit
     ) {
         if (currentUser == null) {
             auth!!.createUserWithEmailAndPassword(email, password)
@@ -90,13 +95,23 @@ class UserManagement : IUserManagement, Observable() {
                         currentUser = auth!!.currentUser
                         setChanged()
                         notifyObservers()
-                        function(true)
+                        result(true)
                     } else {
-                        function(false)
+                        result(false)
                     }
                 }
         }
     }
+
+    override fun loginWithGoogle(intent: Intent, result: (status: Boolean) -> Unit) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(intent)
+        val account: GoogleSignInAccount? = task.getResult(ApiException::class.java)
+        if (account != null) {
+            result(true)
+        }
+        result(false)
+    }
+
 
     override fun resetPassword(function: (status: Boolean) -> Unit) {
         // reference: https://firebase.google.com/docs/auth/android/manage-users
@@ -110,6 +125,18 @@ class UserManagement : IUserManagement, Observable() {
             }
         }
         function(false)
+    }
+
+    override fun resetPasswordByEmail(email: String, function: (status: Boolean) -> Unit) {
+        if (auth != null) {}
+        function(false)
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {task ->
+            if (task.isSuccessful) {
+                function(true)
+            } else {
+                function(false)
+            }
+        }
     }
 
     override fun logout() {
