e9ab452945dfa6c27f1446cef8eee64c9a6747e0
Hrishi <hr581440@dal.ca>
2022-11-26 20:25:22 -0400

    Delete MatchesObject.kt

--- app/src/main/java/com/example/dalsocial/fragment/MatchesObject.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dalsocial.fragment
-
-
-data class MatchesObject(var userId: String, var name: String, var profileImageUrl: String) {
-}
-//Reference: https://github.com/LukeKotlin/tinder-clone-kotlin
