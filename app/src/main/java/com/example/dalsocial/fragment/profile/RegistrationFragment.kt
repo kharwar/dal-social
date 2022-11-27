80460139e1496393f5879024e0e463af0dd62ac5
Hrishi <hr581440@dal.ca>
2022-11-27 14:18:39 -0400

    add Alerts to login and registration fragment, handle registration errors

--- app/src/main/java/com/example/dalsocial/fragment/profile/RegistrationFragment.kt
+++ app/src/main/java/com/example/dalsocial/fragment/profile/RegistrationFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.dalsocial.HomeActivity
@@ -14,6 +13,7 @@ import com.example.dalsocial.R
 import com.example.dalsocial.activity.SetupUserActivity
 import com.example.dalsocial.model.UserManagement
 import com.example.dalsocial.model.UserPersistence
+import com.example.dalsocial.model.states.AuthenticationSuccessState
 import com.google.android.material.textfield.TextInputEditText
 
 class RegistrationFragment : Fragment() {
@@ -40,8 +40,8 @@ class RegistrationFragment : Fragment() {
                 val auth = UserManagement()
                 val persistence = UserPersistence()
 
-                auth.registerWithEmail(email, password) { success ->
-                    if (success) {
+                auth.registerWithEmail(email, password) { state ->
+                    if (state is AuthenticationSuccessState) {
 
                         var intent = Intent(activity, HomeActivity::class.java)
 
@@ -53,7 +53,41 @@ class RegistrationFragment : Fragment() {
                         }
 
                     } else {
-                        Toast.makeText(context, "Registration failed", Toast.LENGTH_SHORT).show()
+                        if (state.message == "The email address is already in use by another account.") {
+                            Toast.makeText(
+                                context,
+                                "The email address is already in use by another account.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(context, "Error: ${state.message}", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        // reference for messages: https://github.com/catapulta-startup-sas/m2colab/blob/6b5deff03ff803b6d0ce08f7e4d5ee8e9f16f507/lib/firebase/handles/sign_up_handle.dart
+                        // got an example message by debugging first, then looked up for that message on GitHuib and got other similar messages
+                        when (state.message) {
+                            "ERROR_EMAIL_ALREADY_IN_USE" -> {
+                                Toast.makeText(
+                                    context,
+                                    "The email is being used by another account.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            "ERROR_INVALID_EMAIL" -> {
+                                Toast.makeText(
+                                    context,
+                                    "Invalid address seems incorrect",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            "ERROR_WEAK_PASSWORD" -> {
+                                Toast.makeText(
+                                    context,
+                                    "Password should have more than 6 characters",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
                     }
                 }
             }
---
+++ app/src/main/java/com/example/dalsocial/model/states/AuthenticationState.kt
@@ -0,0 +1,4 @@
+package com.example.dalsocial.model.states
+
+open class AuthenticationState(var message: String){
+}
